26911360ea08d52173890fee7afd1e42a8a20cc8
AJAY E <ajaye2901@gmail.com>
2024-10-04 14:36:41 +0530

    added new app companyapp

--- jobPortal/jobPortal/urls.py
+++ jobPortal/jobPortal/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('user/', include('userapp.urls'))
+    path('user/', include('userapp.urls')),
+    path('company/', include('companyapp.urls')),
 ]
