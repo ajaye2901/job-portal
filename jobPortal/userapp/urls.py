8741408032b28745a4d88f26242cae21cd45f8b5
AJAY E <ajaye2901@gmail.com>
2024-10-05 13:45:18 +0530

    added admin fns

--- jobPortal/userapp/urls.py
+++ jobPortal/userapp/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path
-from .views import AdminLoginView, UserRegistrationView, UserLoginView
+from .views import *
 
 urlpatterns = [
     path('superuser/login/', AdminLoginView.as_view(), name='admin-login'),
     path('signup/', UserRegistrationView.as_view(), name='signup'),
     path('signin/', UserLoginView.as_view(), name='signin'),
+
+    path('superuser/alljobs/', AdminAllJobsView.as_view(), name='all-jobs'),
+    path('superuser/delete-job/<int:job_id>/', AdminDeleteJobsView.as_view(), name='delete-jobs'),
+    path('superuser/job-applications/', AdminJobApplicationsView.as_view(), name='job-applications'),
+    path('superuser/delete-application/<int:application_id>/', AdminJobApplicationsView.as_view(), name='delete-jobapplications'),
+    path('superuser/allusers/', AllUsersView.as_view(), name='allusers'),
+    
 ]
--- jobPortal/userapp/views.py
+++ jobPortal/userapp/views.py
@@ -7,6 +7,12 @@ from django.contrib.auth import get_user_model
 from rest_framework import status
 from rest_framework.permissions import AllowAny
 from .serializers import UserRegistrationSerializer
+from .permissions import IsAdminUser
+from companyapp.models import JobListing
+from companyapp.serializers import JobListingSerializers
+from django.shortcuts import get_object_or_404
+from candidateapp.models import JobApplication
+from candidateapp.serializers import JobApplicationSerializer
 
 User = get_user_model()
 
@@ -84,3 +90,51 @@ class UserLoginView(APIView):
                 return Response({'error': 'You are not authorized'}, status=status.HTTP_403_FORBIDDEN)
         
         return Response({'error': 'Invalid Credentials'}, status=status.HTTP_400_BAD_REQUEST)
+    
+
+"""
+Admin Functionalities
+
+"""
+
+class AdminAllJobsView(APIView) :
+    permission_classes = [IsAdminUser]
+
+    def get(self, request) :
+        jobs = JobListing.objects.all()
+        serializer = JobListingSerializers(jobs, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+class AdminDeleteJobsView(APIView) :
+    permission_classes = [IsAdminUser]
+
+    def delete(self, request, job_id) :
+        job = get_object_or_404(JobListing, id=job_id)
+        job.delete()
+        return Response({"message" : "Job deleted successfully"}, status=status.HTTP_204_NO_CONTENT)
+    
+class AdminJobApplicationsView(APIView) :
+    permission_classes = [IsAdminUser]
+
+    def get(self, request) :
+        applications = JobApplication.objects.all()
+        serializer = JobApplicationSerializer(applications, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    def delete(self, request, application_id) :
+        application = get_object_or_404(JobApplication, id=application_id)
+        application.delete()
+        return Response({"message" : "job Application Deleted Successfully"}, status=status.HTTP_204_NO_CONTENT)
+    
+class AllUsersView(APIView) :
+    permission_classes = [IsAdminUser]
+
+    def get(self, request) :
+        users = User.objects.exclude(role='Admin')
+        serializer = UserRegistrationSerializer(users, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    def delete(self, request, user_id) :
+        user = get_object_or_404(User, id=user_id)
+        user.delete()
+        return Response({'message' : 'User deleted Successfully'}, status=status.HTTP_204_NO_CONTENT)
